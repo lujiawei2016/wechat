dfb36657769fe7d74d78f9272f338be7ebe099fb
Administrator <Administrator@10.9.13.22>
2017-02-10 18:06:17 +0800

    增加数据库文件

---
+++ src/main/webapp/wechat.sql
@@ -0,0 +1,74 @@
+/*
+SQLyog Ultimate v11.24 (32 bit)
+MySQL - 5.5.20 : Database - test
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`test` /*!40100 DEFAULT CHARACTER SET utf8 */;
+
+USE `test`;
+
+/*Table structure for table `score` */
+
+DROP TABLE IF EXISTS `score`;
+
+CREATE TABLE `score` (
+  `scoreId` int(11) NOT NULL AUTO_INCREMENT,
+  `studentId` varchar(255) DEFAULT NULL,
+  `title` varchar(255) DEFAULT NULL,
+  `china` varchar(255) DEFAULT NULL,
+  `math` varchar(255) DEFAULT NULL,
+  `english` varchar(255) DEFAULT NULL,
+  `growthIntegral` varchar(255) DEFAULT NULL,
+  `active` varchar(255) DEFAULT NULL,
+  `leader` varchar(255) DEFAULT NULL,
+  `createTime` datetime DEFAULT NULL,
+  PRIMARY KEY (`scoreId`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Data for the table `score` */
+
+/*Table structure for table `student` */
+
+DROP TABLE IF EXISTS `student`;
+
+CREATE TABLE `student` (
+  `studentId` varchar(255) NOT NULL,
+  `name` varchar(255) DEFAULT NULL,
+  `phone` varchar(255) DEFAULT NULL,
+  `weixin` varchar(255) DEFAULT NULL,
+  `createTime` datetime DEFAULT NULL,
+  PRIMARY KEY (`studentId`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Data for the table `student` */
+
+insert  into `student`(`studentId`,`name`,`phone`,`weixin`,`createTime`) values ('1','1','1',NULL,'2017-02-10 10:53:00'),('11','11','11',NULL,'2017-02-10 13:54:03'),('2','1','1',NULL,'2017-02-10 10:53:56');
+
+/*Table structure for table `user` */
+
+DROP TABLE IF EXISTS `user`;
+
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(255) DEFAULT NULL,
+  `password` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=37 DEFAULT CHARSET=utf8;
+
+/*Data for the table `user` */
+
+insert  into `user`(`id`,`username`,`password`) values (36,'admin','admin');
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
