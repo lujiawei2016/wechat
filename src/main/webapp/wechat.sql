992562342608d1977e4e17dafcf953b8ba545115
lujiawei <2662493544@qq.com>
2017-02-12 23:59:42 +0800

    增加sql文件和学生模板文件

--- src/main/webapp/wechat.sql
+++ src/main/webapp/wechat.sql
@@ -12,10 +12,6 @@ MySQL - 5.5.20 : Database - test
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-CREATE DATABASE /*!32312 IF NOT EXISTS*/`test` /*!40100 DEFAULT CHARACTER SET utf8 */;
-
-USE `test`;
-
 /*Table structure for table `score` */
 
 DROP TABLE IF EXISTS `score`;
@@ -32,9 +28,13 @@ CREATE TABLE `score` (
   `leader` varchar(255) DEFAULT NULL,
   `createTime` datetime DEFAULT NULL,
   PRIMARY KEY (`scoreId`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8;
 
 /*Data for the table `score` */
+
+LOCK TABLES `score` WRITE;
+
+UNLOCK TABLES;
 
 /*Table structure for table `student` */
 
@@ -51,7 +51,11 @@ CREATE TABLE `student` (
 
 /*Data for the table `student` */
 
-insert  into `student`(`studentId`,`name`,`phone`,`weixin`,`createTime`) values ('1','1','1',NULL,'2017-02-10 10:53:00'),('11','11','11',NULL,'2017-02-10 13:54:03'),('2','1','1',NULL,'2017-02-10 10:53:56');
+LOCK TABLES `student` WRITE;
+
+insert  into `student`(`studentId`,`name`,`phone`,`weixin`,`createTime`) values ('1','卢嘉威','18320172397','o1LrRvuwPFyPhyLayfjuojZQ7r3I','2017-02-12 23:23:10'),('2','卢嘉杰','15112063872',NULL,'2017-02-12 23:23:10'),('3','陈燕红','15018463293',NULL,'2017-02-12 23:23:10'),('4','黎帅斌','13719752671','o1LrRvoxvOYWE_HQwuepuIcqTcJw','2017-02-12 23:35:19');
+
+UNLOCK TABLES;
 
 /*Table structure for table `user` */
 
@@ -66,7 +70,11 @@ CREATE TABLE `user` (
 
 /*Data for the table `user` */
 
+LOCK TABLES `user` WRITE;
+
 insert  into `user`(`id`,`username`,`password`) values (36,'admin','admin');
+
+UNLOCK TABLES;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
 /*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
